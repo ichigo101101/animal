74a9b09527ff267dc1c08cb424dfa41f0dd0b529
ichigo101101 <1429553410@qq.com>
2024-09-09 18:18:22 +0900

    p4 宠物信息的管理

---
+++ springboot/src/main/java/com/example/mapper/AnimalMapper.java
@@ -0,0 +1,34 @@
+package com.example.mapper;
+
+import com.example.entity.Animal;
+
+import java.util.List;
+
+public interface AnimalMapper {
+
+    /**
+     * 新增
+     */
+    int insert(Animal animal);
+
+    /**
+     * 删除
+     */
+    int deleteById(Integer id);
+
+    /**
+     * 修改
+     */
+    int updateById(Animal animal);
+
+    /**
+     * 根据ID查询
+     */
+    Animal selectById(Integer id);
+
+    /**
+     * 查询所有
+     */
+    List<Animal> selectAll(Animal animal);
+
+}
--- vue/src/views/Manager.vue
+++ vue/src/views/Manager.vue
@@ -51,6 +51,7 @@
             </template>
             <el-menu-item index="/admin">管理员信息</el-menu-item>
             <el-menu-item index="/user">用户信息</el-menu-item>
+            <el-menu-item index="/animal">宠物信息</el-menu-item>
           </el-submenu>
         </el-menu>
       </div>
